1143a3c96146f577a5c12c2c16bb55d6332b3ff3
Ashton Bowens <ashxgrey11@gmail.com>
2022-11-02 20:40:55 -0700

    added button

--- server.js
+++ server.js
@@ -15,10 +15,38 @@ app.get('/pokemon', (req, res) => {
     })
 })
 
-
-
-
 //SHOW
+app.put('/pokemon/:id', (req, res) =>{
+    let stats = {
+        hp:req.body.hp,
+        attack: req.body.attack,
+        defense: req.body.defense,
+        spattack: req.body.spattack,
+        spdefense: req.body.spdefense,
+        speed: req.body.speed,
+    }
+    let newPokemon = {
+        name: req.body.name,
+        img: req.body.img,
+        type: req.body.type,
+        stats: stats,
+    }
+    pokemon[req.params.id] = newPokemon
+    res.redirect('/pokemon')
+    console.log(req.body)
+})
+
+//NEW
+app.get('/pokemon/new', (req, res)=>{
+    res.render('new.ejs');
+})
+
+app.get('/pokemon/:id/edit', (req, res) =>{
+    res.render('edit.ejs',{
+        pokemons: pokemon[req.params.id],
+        id: req.params.id,
+    })
+})
 
 
 
