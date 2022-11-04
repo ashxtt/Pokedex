949c286ee75b69011b4f085a236feb174233d508
Ashton Bowens <ashxgrey11@gmail.com>
2022-11-03 22:13:53 -0700

    debugging edit part of this project

--- server.js
+++ server.js
@@ -5,20 +5,20 @@ const methodOverride = require('method-override')
 const pokemon = require('./models/pokemon.js')
 const PORT = 3500
 
+//Middleware
 app.use(express.urlencoded({extended: false}));
 app.use(methodOverride("_method"));
 
 //INDEX
 app.get('/pokemon', (req, res) => {
     res.render('index.ejs', {
-        pokemons: pokemon,
+        allPokemon: pokemon,
     })
 })
 
-//SHOW
-app.put('/pokemon/:id', (req, res) =>{
+app.put('/pokemon/:id', (req, res) => {
     let stats = {
-        hp:req.body.hp,
+        hp: req.body.hp,
         attack: req.body.attack,
         defense: req.body.defense,
         spattack: req.body.spattack,
@@ -48,7 +48,51 @@ app.get('/pokemon/:id/edit', (req, res) =>{
     })
 })
 
+//!Delete
+app.delete('/pokemon/:id', (req, res)=>{
+    pokemon.splice(req.params.id,1)
+    res.redirect('/pokemon')
+})
 
+//?Update
+app.put('/pokemon/:id', (req, res) => {
+    pokemon[req.params.id] = req.params.id
+    res.redirect('/pokemon')
+})
+
+//?Create
+app.post('/pokemon', (req, res)=>{
+    pokemon.push(req.params.id)
+    res.redirect('/pokemon')
+})
+
+//?Edit
+app.get('/pokemon/:id/edit', (req, res)=>{
+    res.render('edit.ejs', {
+        allPokemon: pokemon[req.params.id],
+        id: req.params.id
+    })
+})
+
+//SHOW
+app.post('/pokemon/:id', (req, res) =>{
+    let stats = {
+        hp:req.body.hp,
+        attack: req.body.attack,
+        defense: req.body.defense,
+        spattack: req.body.spattack,
+        spdefense: req.body.spdefense,
+        speed: req.body.speed,
+    }
+    let newPokemon = {
+        name: req.body.name,
+        img: req.body.img,
+        type: req.body.type,
+        stats: stats,
+    }
+    pokemon.push(newPokemon)
+    res.redirect('/pokemon')
+})
 
 //listener
 app.listen(PORT)
