1651d81fc8bfd7717871b0d32f989b1f06191752
Ashton Bowens <ashxgrey11@gmail.com>
2022-11-04 17:56:52 -0700

    finally fineshed debugging another edit a pokemon error

--- server.js
+++ server.js
@@ -5,18 +5,23 @@ const methodOverride = require('method-override')
 const pokemon = require('./models/pokemon.js')
 const PORT = 3500
 
-//Middleware
-app.use(express.urlencoded({extended: false}));
+//Middleware//
+app.use(express.urlencoded({extended : false}));
 app.use(methodOverride("_method"));
 
 //INDEX
 app.get('/pokemon', (req, res) => {
     res.render('index.ejs', {
-        allPokemon: pokemon,
+        pokemons: pokemon,
     })
 })
 
-app.put('/pokemon/:id', (req, res) => {
+//NEW
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+app.post('/pokemon', (req, res) => {
     let stats = {
         hp: req.body.hp,
         attack: req.body.attack,
@@ -31,67 +36,37 @@ app.put('/pokemon/:id', (req, res) => {
         type: req.body.type,
         stats: stats,
     }
-    pokemon[req.params.id] = newPokemon
+    pokemon.push(newPokemon)
     res.redirect('/pokemon')
-    console.log(req.body)
 })
 
-//NEW
-app.get('/pokemon/new', (req, res)=>{
-    res.render('new.ejs');
-})
 
-app.get('/pokemon/:id/edit', (req, res) =>{
-    res.render('edit.ejs',{
-        pokemons: pokemon[req.params.id],
-        id: req.params.id,
-    })
-})
 
-//!Delete
-app.delete('/pokemon/:id', (req, res)=>{
+//DELETE
+app.delete('/pokemon/:id', (req, res) => {
     pokemon.splice(req.params.id,1)
     res.redirect('/pokemon')
 })
 
-//?Update
-app.put('/pokemon/:id', (req, res) => {
-    pokemon[req.params.id] = req.params.id
-    res.redirect('/pokemon')
+//UPDATE
+app.put("/pokemon/:id", (req, res) => {
+    pokemons: pokemon[req.params.id] = req.params.id
+    res.redirect("/pokemon")
 })
 
-//?Create
-app.post('/pokemon', (req, res)=>{
-    pokemon.push(req.params.id)
-    res.redirect('/pokemon')
-})
-
-//?Edit
-app.get('/pokemon/:id/edit', (req, res)=>{
+//EDIT
+app.get('/pokemon/:id/edit', (req, res) => {
     res.render('edit.ejs', {
-        allPokemon: pokemon[req.params.id],
-        id: req.params.id
+        pokemons: pokemon[req.params.id],
+        id: req.params.id,
     })
+    
 })
 
-//SHOW
-app.post('/pokemon/:id', (req, res) =>{
-    let stats = {
-        hp:req.body.hp,
-        attack: req.body.attack,
-        defense: req.body.defense,
-        spattack: req.body.spattack,
-        spdefense: req.body.spdefense,
-        speed: req.body.speed,
-    }
-    let newPokemon = {
-        name: req.body.name,
-        img: req.body.img,
-        type: req.body.type,
-        stats: stats,
-    }
-    pokemon.push(newPokemon)
-    res.redirect('/pokemon')
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', {
+        pokemons: pokemon[req.params.id],
+    })
 })
 
 //listener
