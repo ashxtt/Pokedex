05f7ea59a2729e731bc9df209565f006b88d9901
Ashton Bowens <ashxgrey11@gmail.com>
2022-11-08 10:17:26 -0800

    debugging put route

--- server.js
+++ server.js
@@ -21,6 +21,26 @@ app.get('/pokemon/new', (req, res) => {
     res.render('new.ejs')
 })
 
+//PUT ROUTE TO UPDATE DATABASE
+app.put('/pokemon/:id', (res, req)=>{
+    let stats = {
+        hp: req.body.hp,
+        attack: req.body.attack,
+        defense: req.body.defense,
+        speed: req.body.speed,
+    }
+    let newPokemon = {
+        name: req.body.name,
+        img: req.body.img,
+        type: req.body.type,
+        stats: stats,
+    }
+    //pass params
+    pokemon[req.params.id] = newPokemon
+    res.redirect('/pokemon')
+    
+})
+
 //CREATE
 app.post('/pokemon', (req, res) => {
     let stats = {
